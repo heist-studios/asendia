54cc06afa7b5ad7acdb369de85d869ec11b4715b
Tim Davies <tim@heist-studios.com>
2017-07-13 15:40:12 +0100

    Store authentication details, and update README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# asendia
+# Asendia
+
 Ruby API wrapper for Asendia SOAP API
+
+## Getting Started
+
+Create an Asendia::Client object with your username and password:
+
+```
+  asendia = Asendia::Client.new(
+    username: 'foo',
+    password: 'bar',
+    live:     Rails.env.production?
+  )
+```
+
--- lib/asendia/client.rb
+++ lib/asendia/client.rb
@@ -1,13 +1,19 @@
 module Asendia
   # Handles communication with Asendia API
   class Client
-    def get_order(order_id)
+    def initialize(username:, password:, live: false)
+      @username = username
+      @password = password
+      @live     = live
     end
 
-    def get_stock_update(order_ids)
-    end
+    # def get_order(order_id)
+    # end
 
-    def create_order(attributes)
-    end
+    # def get_stock_update(order_ids)
+    # end
+
+    # def create_order(attributes)
+    # end
   end
 end
